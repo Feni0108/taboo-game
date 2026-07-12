bc69005ad8ca5ebd757fbc92c39e9e2684a0ef95
Feni0108 <94fanni@gmail.com>
2026-07-12 20:14:49 +0200

    create some basic cards

---
+++ src/cards.js
@@ -0,0 +1,60 @@
+const cards = [
+  {
+    word: "Ananász",
+    taboo: ["Gyümölcs", "Lé", "Mar", "Konzerv", "Koktél"],
+  },
+  {
+    word: "Egér",
+    taboo: ["Számítógép", "Rágcsáló", "Cincog", "Bajusz", "Jerry"],
+  },
+  {
+    word: "Ebéd",
+    taboo: ["Étkezés", "Szünet", "Családi", "Dél", "Enni"],
+  },
+  {
+    word: "Számla",
+    taboo: ["Blokk", "Díj", "Fizet", "Szolgáltatás", "Vásárlás"],
+  },
+  {
+    word: "Édesség",
+    taboo: ["Cukor", "Íz", "Finom", "Nassi", "Desszert"],
+  },
+  {
+    word: "Studió",
+    taboo: ["Művészet", "Kiállítás", "Tánc", "Apartman", "Galéria"],
+  },
+  {
+    word: "Héj",
+    taboo: ["Banán", "Gyümölcs", "Pucolni", "Bőr", "Narancs"],
+  },
+  {
+    word: "Diéta",
+    taboo: ["Kalória", "Vékony", "Fogyni", "Súly", "Mérleg"],
+  },
+  {
+    word: "Barack",
+    taboo: ["Puha", "Lé", "Gyümölcs", "Kajszi", "Mag"],
+  },
+  {
+    word: "Tej",
+    taboo: ["Kecske", "Tehén", "Laktóz", "Fehér", "Ital"],
+  },
+  {
+    word: "Amnézia",
+    taboo: ["Emlék", "Felejteni", "Fej", "Agy", "Sérülés"],
+  },
+  {
+    word: "Milliómos",
+    taboo: ["Gazdag", "Vagyon", "Pénz", "Lottó", "Jacht"],
+  },
+  {
+    word: "Pizza",
+    taboo: ["Paradicsomszósz", "Kemence", "Olaszország", "Sajt", "Kerek"],
+  },
+  {
+    word: "Kutya",
+    taboo: ["Háziállat", "Csont", "Ugat", "Póráz", "Szőrös"],
+  },
+];
+
+export default cards;
