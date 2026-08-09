5340c3ff8923a9beae6ae4c57a4dfba519479b84
Feni0108 <94fanni@gmail.com>
2026-08-09 19:52:32 +0200

    adding new words to the game

--- src/cards.js
+++ src/cards.js
@@ -1,60 +1,504 @@
 const cards = [
   {
-    word: "Ananász",
-    taboo: ["Gyümölcs", "Lé", "Mar", "Konzerv", "Koktél"],
+    "word": "Ananász",
+    "taboo": [
+      "Gyümölcs",
+      "Lé",
+      "Mar",
+      "Konzerv",
+      "Koktél"
+    ]
   },
   {
-    word: "Egér",
-    taboo: ["Számítógép", "Rágcsáló", "Cincog", "Bajusz", "Jerry"],
+    "word": "Egér",
+    "taboo": [
+      "Számítógép",
+      "Rágcsáló",
+      "Cincog",
+      "Bajusz",
+      "Jerry"
+    ]
   },
   {
-    word: "Ebéd",
-    taboo: ["Étkezés", "Szünet", "Családi", "Dél", "Enni"],
+    "word": "Ebéd",
+    "taboo": [
+      "Étkezés",
+      "Szünet",
+      "Családi",
+      "Dél",
+      "Enni"
+    ]
   },
   {
-    word: "Számla",
-    taboo: ["Blokk", "Díj", "Fizet", "Szolgáltatás", "Vásárlás"],
+    "word": "Számla",
+    "taboo": [
+      "Blokk",
+      "Díj",
+      "Fizet",
+      "Szolgáltatás",
+      "Vásárlás"
+    ]
   },
   {
-    word: "Édesség",
-    taboo: ["Cukor", "Íz", "Finom", "Nassi", "Desszert"],
+    "word": "Édesség",
+    "taboo": [
+      "Cukor",
+      "Íz",
+      "Finom",
+      "Nassi",
+      "Desszert"
+    ]
   },
   {
-    word: "Studió",
-    taboo: ["Művészet", "Kiállítás", "Tánc", "Apartman", "Galéria"],
+    "word": "Studió",
+    "taboo": [
+      "Művészet",
+      "Kiállítás",
+      "Tánc",
+      "Apartman",
+      "Galéria"
+    ]
   },
   {
-    word: "Héj",
-    taboo: ["Banán", "Gyümölcs", "Pucolni", "Bőr", "Narancs"],
+    "word": "Héj",
+    "taboo": [
+      "Banán",
+      "Gyümölcs",
+      "Pucolni",
+      "Bőr",
+      "Narancs"
+    ]
   },
   {
-    word: "Diéta",
-    taboo: ["Kalória", "Vékony", "Fogyni", "Súly", "Mérleg"],
+    "word": "Diéta",
+    "taboo": [
+      "Kalória",
+      "Vékony",
+      "Fogyni",
+      "Súly",
+      "Mérleg"
+    ]
   },
   {
-    word: "Barack",
-    taboo: ["Puha", "Lé", "Gyümölcs", "Kajszi", "Mag"],
+    "word": "Barack",
+    "taboo": [
+      "Puha",
+      "Lé",
+      "Gyümölcs",
+      "Kajszi",
+      "Mag"
+    ]
   },
   {
-    word: "Tej",
-    taboo: ["Kecske", "Tehén", "Laktóz", "Fehér", "Ital"],
+    "word": "Tej",
+    "taboo": [
+      "Kecske",
+      "Tehén",
+      "Laktóz",
+      "Fehér",
+      "Ital"
+    ]
   },
   {
-    word: "Amnézia",
-    taboo: ["Emlék", "Felejteni", "Fej", "Agy", "Sérülés"],
+    "word": "Amnézia",
+    "taboo": [
+      "Emlék",
+      "Felejteni",
+      "Fej",
+      "Agy",
+      "Sérülés"
+    ]
   },
   {
-    word: "Milliómos",
-    taboo: ["Gazdag", "Vagyon", "Pénz", "Lottó", "Jacht"],
+    "word": "Milliómos",
+    "taboo": [
+      "Gazdag",
+      "Vagyon",
+      "Pénz",
+      "Lottó",
+      "Jacht"
+    ]
   },
   {
-    word: "Pizza",
-    taboo: ["Paradicsomszósz", "Kemence", "Olaszország", "Sajt", "Kerek"],
+    "word": "Pizza",
+    "taboo": [
+      "Paradicsomszósz",
+      "Kemence",
+      "Olaszország",
+      "Sajt",
+      "Kerek"
+    ]
   },
   {
-    word: "Kutya",
-    taboo: ["Háziállat", "Csont", "Ugat", "Póráz", "Szőrös"],
+    "word": "Kutya",
+    "taboo": [
+      "Háziállat",
+      "Csont",
+      "Ugat",
+      "Póráz",
+      "Szőrös"
+    ]
   },
+  {
+    "word": "Maradék",
+    "taboo": [
+      "Újramelegít",
+      "Tegnap",
+      "Étel",
+      "Ebéd",
+      "Tárol"
+    ]
+  },
+  {
+    "word": "Rágni",
+    "taboo": [
+      "Fogak",
+      "étel",
+      "enni",
+      "Csámcsogni",
+      "Száj"
+    ]
+  },
+  {
+    "word": "Banán",
+    "taboo": [
+      "gyümölcs",
+      "sárga",
+      "hámozni",
+      "majom",
+      "íves"
+    ]
+  },
+  {
+    "word": "Denevér",
+    "taboo": [
+      "repül",
+      "batman",
+      "vámpír",
+      "éjszaka",
+      "állat"
+    ]
+  },
+  {
+    "word": "Pók",
+    "taboo": [
+      "nyolc",
+      "láb",
+      "rovar",
+      "fekete özvegy",
+      "háló"
+    ]
+  },
+  {
+    "word": "Pékség",
+    "taboo": [
+      "kenyér",
+      "sütemény",
+      "tészta",
+      "kakakós csiga",
+      "friss"
+    ]
+  },
+  {
+    "word": "Fűnyíró",
+    "taboo": [
+      "kert",
+      "vágni",
+      "fű",
+      "tolni",
+      "penge"
+    ]
+  },
+  {
+    "word": "Hűtő",
+    "taboo": [
+      "jég",
+      "fagyasztó",
+      "hideg",
+      "frissen tartó",
+      "étel"
+    ]
+  },
+  {
+    "word": "Körte",
+    "taboo": [
+      "gyümölcs",
+      "fa",
+      "lé",
+      "enni",
+      "sárga"
+    ]
+  },
+  {
+    "word": "Tó",
+    "taboo": [
+      "víz",
+      "oceán",
+      "horgászni",
+      "nagy",
+      "úszni"
+    ]
+  },
+  {
+    "word": "Tanóra",
+    "taboo": [
+      "beszéd",
+      "előadás",
+      "tanítás",
+      "irányítás",
+      "diákok"
+    ]
+  },
+  {
+    "word": "Rózsaszín",
+    "taboo": [
+      "szín",
+      "piros",
+      "lány",
+      "Pink",
+      "baba"
+    ]
+  },
+  {
+    "word": "Napló",
+    "taboo": [
+      "írni",
+      "magazin",
+      "újság",
+      "cikk",
+      "naptár"
+    ]
+  },
+  {
+    "word": "Bögre",
+    "taboo": [
+      "inni",
+      "csésze",
+      "tea",
+      "kakaó",
+      "kávé"
+    ]
+  },
+  {
+    "word": "Vámpír",
+    "taboo": [
+      "transzilvánia",
+      "drakula",
+      "vér",
+      "agyar",
+      "koporsó"
+    ]
+  },
+  {
+    "word": "Oliva olaj",
+    "taboo": [
+      "főzni",
+      "önteni",
+      "szűz",
+      "préselt",
+      "bogyó"
+    ]
+  },
+  {
+    "word": "Krém",
+    "taboo": [
+      "kávé",
+      "sajt",
+      "tej",
+      "jég",
+      "hab"
+    ]
+  },
+  {
+    "word": "Kalória",
+    "taboo": [
+      "számlálás",
+      "égetés",
+      "alacsony",
+      "deficit",
+      "súly"
+    ]
+  },
+  {
+    "word": "Aroma",
+    "taboo": [
+      "illat",
+      "szag",
+      "orr",
+      "bűz",
+      "párologtat"
+    ]
+  },
+  {
+    "word": "Piknik",
+    "taboo": [
+      "kosár",
+      "étel",
+      "kint",
+      "enni",
+      "hangyák"
+    ]
+  },
+  {
+    "word": "Állatorvos",
+    "taboo": [
+      "vizsgál",
+      "beteg",
+      "kutya",
+      "oltás",
+      "gyógyít"
+    ]
+  },
+  {
+    "word": "Pökrölt",
+    "taboo": [
+      "étel",
+      "lábos",
+      "hagyma",
+      "magyaros",
+      "paprika"
+    ]
+  },
+  {
+    "word": "Helikopter",
+    "taboo": [
+      "lapát",
+      "utazni",
+      "repülni",
+      "chopper",
+      "mentő"
+    ]
+  },
+  {
+    "word": "Popcorn",
+    "taboo": [
+      "kukorica",
+      "tál",
+      "vajas",
+      "mikró",
+      "mozi"
+    ]
+  },
+  {
+    "word": "Leves",
+    "taboo": [
+      "paradicsom",
+      "csirke",
+      "kanál",
+      "zöldség",
+      "hús"
+    ]
+  },
+  {
+    "word": "Vaj",
+    "taboo": [
+      "margarin",
+      "kenni",
+      "pirítós",
+      "tejtermék",
+      "sütni"
+    ]
+  },
+  {
+    "word": "Tészta",
+    "taboo": [
+      "pizza",
+      "olasz",
+      "spagetti",
+      "szósz",
+      "étel"
+    ]
+  },
+  {
+    "word": "Macska",
+    "taboo": [
+      "nyávog",
+      "simogatni",
+      "szőrcsomó",
+      "Tom",
+      "nő"
+    ]
+  },
+  {
+    "word": "Elefánt",
+    "taboo": [
+      "afrika",
+      "nagy",
+      "állat",
+      "ormány",
+      "Dumbo"
+    ]
+  },
+  {
+    "word": "Oroszlán",
+    "taboo": [
+      "Szimba",
+      "király",
+      "macska",
+      "karom",
+      "ragadozó"
+    ]
+  },
+  {
+    "word": "Tigris",
+    "taboo": [
+      "csíkos",
+      "Sirkán",
+      "macska",
+      "narancssárga",
+      "dzsungel"
+    ]
+  },
+  {
+    "word": "Zsiráf",
+    "taboo": [
+      "hosszú",
+      "nyak",
+      "foltos",
+      "sárga",
+      "növényevő"
+    ]
+  },
+  {
+    "word": "Zebra",
+    "taboo": [
+      "ló",
+      "csíkos",
+      "fekete-fehér",
+      "pata",
+      "legel"
+    ]
+  },
+  {
+    "word": "Majom",
+    "taboo": [
+      "ember",
+      "rokon",
+      "banán",
+      "csimpánz",
+      "ugrál"
+    ]
+  },
+  {
+    "word": "Medve",
+    "taboo": [
+      "románia",
+      "erdő",
+      "barna",
+      "lazac",
+      "téli álom"
+    ]
+  },
+  {
+    "word": "Farkas",
+    "taboo": [
+      "kutya",
+      "falka",
+      "ragadozó",
+      "vonyítás",
+      "vad"
+    ]
+  }
 ];
 
 export default cards;
